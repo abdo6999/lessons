0eddd9f3fa0f7ad204b2f48107b31282f8fa424b
abdelrahman <https://github.com/abdo6999>
2023-08-24 17:15:17 +0300

    update

---
+++ lesson 1/lessons.py
@@ -0,0 +1,50 @@
+# Lesson 1: Introduction to Python
+# - Learn what Python is and why it's popular.
+# - Install Python and a code editor if you haven't already.
+
+# Lesson 2: Variables and Data Types
+# - Understand how to declare variables.
+# - Explore data types like int, float, str, and bool.
+
+# Lesson 3: Basic Input and Output
+# - Learn how to take user input and display output using print().
+
+# Lesson 4: Conditional Statements
+# - Explore if, elif, and else statements for decision-making.
+
+# Lesson 5: Loops
+# - Learn about for and while loops for repetitive tasks.
+
+# Lesson 6: Lists and Tuples
+# - Understand how to create, access, and modify lists and tuples.
+
+# Lesson 7: Dictionaries and Sets
+# - Explore dictionaries and sets for storing key-value pairs and unique values.
+
+# Lesson 8: Functions
+# - Define and use functions to organize your code.
+
+# Lesson 9: Modules and Libraries
+# - Learn how to import and use external modules and libraries.
+
+# Lesson 10: File Handling
+# - Read and write data to files using Python.
+
+# Lesson 11: Exception Handling
+# - Understand how to handle errors and exceptions gracefully.
+
+# Lesson 12: Object-Oriented Programming (OOP)
+# - Learn the basics of classes and objects.
+
+# Lesson 13: Advanced Topics (Choose one or more depending on your interests):
+#   - Regular Expressions
+#   - Working with APIs
+#   - Web Scraping
+#   - Data Analysis with Pandas
+#   - GUI Development (e.g., Tkinter)
+
+# Lesson 14: Final Projects
+# - Apply your knowledge to create small projects or scripts.
+
+# Lesson 15: Additional Resources and Practice
+# - Explore online tutorials, exercises, and coding challenges to further improve your skills.
